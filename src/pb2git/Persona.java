7f31bc37b96fd350744509411574546767320f11
unknown <aborgeat@unlam.edu.ar>
2020-09-15 20:19:08 -0300

    Primer commit

---
+++ src/pb2git/Persona.java
@@ -0,0 +1,13 @@
+package pb2git;
+
+public class Persona {
+private String nombre;
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+}
